0dc7496e7563c7d8eeb7ffb16b937d22afd39284
Willian Melo <willian.vieirademelo@gmail.com>
2024-08-16 00:30:13 -0300

    Feat: implementandando api suppliers

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Supplier
+from .serializers import SupplierSerializer
 
-# Create your views here.
+class SupplierFilterView(APIView):
+    def post(self, request):
+        consumption = request.data.get('consumption')
+        if consumption is not None:
+            suppliers = Supplier.objects.filter(min_kwh_limit__lte=consumption)
+            serializer = SupplierSerializer(suppliers, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"error": "Consumo não informado."}, status=status.HTTP_400_BAD_REQUEST)
