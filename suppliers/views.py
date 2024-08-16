4e4ad9ddfb750167c4eb242d5d92a4c11347dee6
Willian Melo <willian.vieirademelo@gmail.com>
2024-08-16 01:04:15 -0300

    Fix: incluindo verificação api

--- suppliers/views.py
+++ suppliers/views.py
@@ -6,9 +6,17 @@ from .serializers import SupplierSerializer
 
 class SupplierFilterView(APIView):
     def post(self, request):
+
         consumption = request.data.get('consumption')
+        
         if consumption is not None:
-            suppliers = Supplier.objects.filter(min_kwh_limit__lte=consumption)
+            
+            suppliers = Supplier.objects.filter(min_kwh_limit__lt=consumption)
             serializer = SupplierSerializer(suppliers, many=True)
+
+            if not serializer.data:
+                return Response({"error": "Nenhum fornecedor corresponde ao filtro"}, status=status.HTTP_404_NOT_FOUND)
+            
             return Response(serializer.data, status=status.HTTP_200_OK)
+        
         return Response({"error": "Consumo não informado."}, status=status.HTTP_400_BAD_REQUEST)
