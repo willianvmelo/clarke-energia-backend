5a5774aaefe9077b5bdec131bb67235a9f11a843
Willian Melo <willian.vieirademelo@gmail.com>
2024-08-17 07:47:56 -0300

    Feat: implementando endpoint para criar fornecedor.

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SupplierFilterView
+from .views import SupplierFilterView, SupplierCreateView
 
 urlpatterns = [
     path('filter-suppliers/', SupplierFilterView.as_view(), name='filter_suppliers'),
+    path('suppliers/create/', SupplierCreateView.as_view(), name='supplier-create'),
 ]
